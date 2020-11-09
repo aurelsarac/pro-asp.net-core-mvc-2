89cd801146375d6ce535b5590111b6527206107e
aurelsarac <e-business@aurelonline.com>
2020-11-09 09:51:35 +0200

    Updated HomeController

--- 02 - Your First MVC Application/PartyInvites/WebApplication1/Controllers/HomeController.cs
+++ 02 - Your First MVC Application/PartyInvites/WebApplication1/Controllers/HomeController.cs
@@ -11,23 +11,24 @@ namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger) {
-            _logger = logger;
+        //private readonly ILogger<HomeController> _logger;
+
+        //public HomeController(ILogger<HomeController> logger) {
+        //    _logger = logger;
+        //}
+
+        public String  Index() {
+            return "HelloWorld";
         }
 
-        public IActionResult Index() {
-            return View();
-        }
+        //public IActionResult Privacy() {
+        //    return View();
+        //}
 
-        public IActionResult Privacy() {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error() {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error() {
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
     }
 }
